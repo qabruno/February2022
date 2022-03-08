a6936a4b9060b2ae90981b6877594dd457ee57f8
Bruno <bmacedo1987@gmail.com>
2022-03-08 21:11:51 +1300

    Specflow implementation

---
+++ StepDefinitions/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace February2022.StepDefinitions
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given(@"I logged into TurnUp portal successfully")]
+        public void GivenILoggedIntoTurnUpPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I navigate to time and material page")]
+        public void GivenINavigateToTimeAndMaterialPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I create time and material record")]
+        public void WhenICreateTimeAndMaterialRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the record should be created successfully")]
+        public void ThenTheRecordShouldBeCreatedSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
